b62de8c2424a10a03556f32779a9a294472039f0
luoye <luoyefe@gmail.com>
2017-02-23 09:57:58 +0800

    check exist or not

--- format.py
+++ format.py
@@ -5,12 +5,16 @@ import json;
 
 result = {};
 
+with open('./icons.json', 'r') as f:
+	data = json.load(f);
+
 for root, dirs, files in os.walk('./svg'):
 	for file in files:
-		result[file] = '';
+		if not data[file]:
+			data[file] = '';
 
 fo = open('./icons.json', 'r+');
 
-fo.write(json.dumps(result, indent = 4, sort_keys=True));
+fo.write(json.dumps(data, indent = 4, sort_keys=True));
 
 fo.close();
